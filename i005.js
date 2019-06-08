be272fad2ad1902e66b777149e509a1506869b0a
xiangguo <guo.xiang@56qq.com>
2019-06-08 11:58:52 +0800

    个人资料

--- i005.js
+++ i005.js
@@ -4,6 +4,11 @@ iweb.controller('i005', function($scope,$routeParams) {
     $scope.changeTab=function (tabIndex) {
         this.tabIndex=tabIndex
     }
+    $scope.toLive =function(url,talk,status){
+        // var liveUrl = "#/i101?gid="+talk+"&u="+url+"&s="+status;
+        var liveUrl = "#/i101?gid=o15598664532377779483&u=rtmp://dev.pull.live.121tongbu.com/live/XtbjfRaUg49D2kWcR2&s=true";
+        window.open(liveUrl,'_blank');
+    }
     if(apiconn.conn_state==='IN_SESSION'){
         ajax({obj:'user',act:'mycoursedetail',course_id:$routeParams.id},function (data) {
             $scope.subject=data.info
