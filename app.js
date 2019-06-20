c75d0c9a7e5b4c0fb10272e61a63a7f0b26f9d9a
xiangguo <guo.xiang@56qq.com>
2019-06-21 00:04:47 +0800

    第一版完工

--- app.js
+++ app.js
@@ -1,234 +1,235 @@
 // Based on AngularJS 1.4.2
 
-var IWEB_ACCOUNT    = "test2";
+var IWEB_ACCOUNT = "test2";
 var TOOLBOX_ACCOUNT = "test1";
 
 var iweb = angular.module('iweb', ['ngRoute']);
-window.iweb=iweb
+window.iweb = iweb
 
 iweb.config(['$routeProvider',
-  	function($routeProvider) {
+  	function ($routeProvider) {
 
-    		$routeProvider.
-            when('/i200', {
-                templateUrl: 'i200.html',
-                controller: 'i200'
-            }).
-            when('/i201', {
-                templateUrl: 'i201.html',
-                controller: 'i201'
-            }).
-            when('/i202', {
-                templateUrl: 'i202.html',
-                controller: 'i202'
-            }).
-            when('/i203', {
-                templateUrl: 'i203.html',
-                controller: 'i203'
-            }).
-      		when('/i000', {
-    			templateUrl: 'i000.html',
-    			controller: 'i000'
-      		}).
-      		when('/i072', {
-    			templateUrl: 'i072.html',
-    			controller: 'i072'
-      		}).
-      		when('/i001', {
-    			templateUrl: 'i003.html',
-    			controller: 'i003'
-      		}).
-      		when('/main', {
-    			templateUrl: 'main.html',
-    			controller: 'main'
-      		}).
-			when('/i002', {
-				templateUrl: 'i002.html',//选课中心
-				controller: 'i002'
-			}).
-            when('/i003', {
-                templateUrl: 'i003.html',//个人资料
-                controller: 'i003'
-            }).
-            when("/i100",{
-            	templateUrl: 'i100.html',
-                controller: 'i100'
-            }).
-            when("/i101",{
-            	templateUrl: 'i101.html',
-            	controller: 'i101'
-            }).
-            when('/i004', {
-                templateUrl: 'i004.html',//课程列表
-                controller: 'i004'
-            }).
-			when('/i005', {
-				templateUrl: 'i005.html',//课程详情
-				controller: 'i005'
-			}).
-            when('/i006', {
-                templateUrl: 'i006.html',//订单列表
-                controller: 'i006'
-            }).
-            when('/i007', {
-                templateUrl: 'i007.html',//订单详情
-                controller: 'i007'
-            }).
-            when('/i008', {
-                templateUrl: 'i008.html',//习题
-                controller: 'i008'
-            }).
-            when('/i009', {
-                templateUrl: 'i009.html',//班级列表
-                controller: 'i009'
-            }).
-            when('/i010', {
-                templateUrl: 'i010.html',//班级详情
-                controller: 'i010'
-            }).
-            when('/i011', {
-                templateUrl: 'i011.html',//立即报名
-                controller: 'i011'
-            }).
-            when('/i012', {
-                templateUrl: 'i012.html',//立即报名
-                controller: 'i012'
-            }).
-            when('/i013', {
-                templateUrl: 'i013.html',//我的优惠券
-                controller: 'i013'
-            }).
-            when('/i014', {
-                templateUrl: 'i014.html',//我的优惠券
-                controller: 'i014'
-            }).
-            when('/i015', {
-                templateUrl: 'i015.html',//错题本
-                controller: 'i015'
-            }).
-            when('/i016', {
-                templateUrl: 'i016.html',//搜索结果
-                controller: 'i016'
-            }).
-      		otherwise({
-    			redirectTo: '/i203'
-      		});
+        $routeProvider.
+        when('/i200', {
+            templateUrl: 'i200.html',
+            controller: 'i200'
+        }).
+        when('/i201', {
+            templateUrl: 'i201.html',
+            controller: 'i201'
+        }).
+        when('/i202', {
+            templateUrl: 'i202.html',
+            controller: 'i202'
+        }).
+        when('/i203', {
+            templateUrl: 'i203.html',
+            controller: 'i203'
+        }).
+        when('/i000', {
+            templateUrl: 'i000.html',
+            controller: 'i000'
+        }).
+        when('/i072', {
+            templateUrl: 'i072.html',
+            controller: 'i072'
+        }).
+        when('/i001', {
+            templateUrl: 'i003.html',
+            controller: 'i003'
+        }).
+        when('/main', {
+            templateUrl: 'main.html',
+            controller: 'main'
+        }).
+        when('/i002', {
+            templateUrl: 'i002.html', //选课中心
+            controller: 'i002'
+        }).
+        when('/i003', {
+            templateUrl: 'i003.html', //个人资料
+            controller: 'i003'
+        }).
+        when("/i100", {
+            templateUrl: 'i100.html',
+            controller: 'i100'
+        }).
+        when("/i101", {
+            templateUrl: 'i101.html',
+            controller: 'i101'
+        }).
+        when('/i004', {
+            templateUrl: 'i004.html', //课程列表
+            controller: 'i004'
+        }).
+        when('/i005', {
+            templateUrl: 'i005.html', //课程详情
+            controller: 'i005'
+        }).
+        when('/i006', {
+            templateUrl: 'i006.html', //订单列表
+            controller: 'i006'
+        }).
+        when('/i007', {
+            templateUrl: 'i007.html', //订单详情
+            controller: 'i007'
+        }).
+        when('/i008', {
+            templateUrl: 'i008.html', //习题
+            controller: 'i008'
+        }).
+        when('/i009', {
+            templateUrl: 'i009.html', //班级列表
+            controller: 'i009'
+        }).
+        when('/i010', {
+            templateUrl: 'i010.html', //班级详情
+            controller: 'i010'
+        }).
+        when('/i011', {
+            templateUrl: 'i011.html', //立即报名
+            controller: 'i011'
+        }).
+        when('/i012', {
+            templateUrl: 'i012.html', //立即报名
+            controller: 'i012'
+        }).
+        when('/i013', {
+            templateUrl: 'i013.html', //我的优惠券
+            controller: 'i013'
+        }).
+        when('/i014', {
+            templateUrl: 'i014.html', //我的优惠券
+            controller: 'i014'
+        }).
+        when('/i015', {
+            templateUrl: 'i015.html', //错题本
+            controller: 'i015'
+        }).
+        when('/i016', {
+            templateUrl: 'i016.html', //搜索结果
+            controller: 'i016'
+        }).
+        otherwise({
+            redirectTo: '/i203'
+        });
 }]);
 
 // save a handle to the $rootScope obj
 var rootScope;
-var callBackFn={}
+var callBackFn = {}
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function goto_view(v) {
     $(window).scrollTop(0);
-  	var baseUrl = window.location.href;
-	baseUrl = (baseUrl.indexOf('#') > 0 ? baseUrl.substr(0, baseUrl.indexOf('#')) : baseUrl);
-	window.location.href = baseUrl + "#/" + v;
+    var baseUrl = window.location.href;
+    baseUrl = (baseUrl.indexOf('#') > 0 ? baseUrl.substr(0, baseUrl.indexOf('#')) : baseUrl);
+    window.location.href = baseUrl + "#/" + v;
 }
 
 function logout() {
-	sessionStorage.setItem("login_name", "");
-	sessionStorage.setItem("login_passwd", "");
-	apiconn.logout();
+    sessionStorage.setItem("login_name", "");
+    sessionStorage.setItem("login_passwd", "");
+    apiconn.logout();
 }
 
 var apiconn = new APIConnection();
-window.ajax=function(params,cb){
-	if(cb){
-	    if(callBackFn[params.obj+'_'+params.act]){
-            callBackFn[params.obj+'_'+params.act].push(cb)
-        }else{
-            callBackFn[params.obj+'_'+params.act]=[cb]
+window.ajax = function (params, cb) {
+    if (cb) {
+        if (callBackFn[params.obj + '_' + params.act]) {
+            callBackFn[params.obj + '_' + params.act].push(cb)
+        } else {
+            callBackFn[params.obj + '_' + params.act] = [cb]
         }
-        }
+    }
     apiconn.send_obj(params)
 }
 apiconn.client_info.clienttype = "web";
 
-apiconn.state_changed_handler = function() {
-	rootScope.$apply(function() {
+apiconn.state_changed_handler = function () {
+    rootScope.$apply(function () {
 
-		console.log("state: "+apiconn.from_state+" => "+apiconn.conn_state);
+        console.log("state: " + apiconn.from_state + " => " + apiconn.conn_state);
 
 
-		if (apiconn.conn_state == "IN_SESSION") {
-			sessionStorage.setItem("login_name", apiconn.login_name);
-			sessionStorage.setItem("login_passwd", apiconn.login_passwd);
-            sessionStorage.setItem("credential_data",JSON.stringify(apiconn.credential_data))
+        if (apiconn.conn_state == "IN_SESSION") {
+            sessionStorage.setItem("login_name", apiconn.login_name);
+            sessionStorage.setItem("login_passwd", apiconn.login_passwd);
+            sessionStorage.setItem("credential_data", JSON.stringify(apiconn.credential_data))
             // window.location.reload()
-		} else if (apiconn.conn_state == "LOGIN_SCREEN_ENABLED") {
+        } else if (apiconn.conn_state == "LOGIN_SCREEN_ENABLED") {
 
-			// auto re login after page refresh
+            // auto re login after page refresh
 
-			if (apiconn.login_name == "" && apiconn.credential_data == null) {
+            if (apiconn.login_name == "" && apiconn.credential_data == null) {
 
-				var login_name = sessionStorage.getItem("login_name");
-	            		var login_passwd = sessionStorage.getItem("login_passwd");
+                var login_name = sessionStorage.getItem("login_name");
+                var login_passwd = sessionStorage.getItem("login_passwd");
 
-				var cred = sessionStorage.getItem("credential_data");
-				var cred_obj = null;
-				console.log(cred,'sssssssssssssssssssssssssssss')
-				if (cred !== "") cred_obj = JSON.parse(cred);
+                var cred = sessionStorage.getItem("credential_data");
+                var cred_obj = null;
+                console.log(cred, 'sssssssssssssssssssssssssssss')
+                if (cred !== "") cred_obj = JSON.parse(cred);
                 // alert('s')
-				// reset stored cred to prevent infinite loop in case of failure
-				sessionStorage.setItem("login_name", "");
-				sessionStorage.setItem("login_passwd", "");
-				sessionStorage.setItem("credential_data", "");
+                // reset stored cred to prevent infinite loop in case of failure
+                sessionStorage.setItem("login_name", "");
+                sessionStorage.setItem("login_passwd", "");
+                sessionStorage.setItem("credential_data", "");
 
-				if (login_name != "" && login_name != null) {
-	                		apiconn.login(login_name, login_passwd);
+                if (login_name != "" && login_name != null) {
+                    apiconn.login(login_name, login_passwd);
 
-				} else if (cred_obj != null) {
-	                		apiconn.loginx(cred_obj);
+                } else if (cred_obj != null) {
+                    apiconn.loginx(cred_obj);
 
-				} else {
-				}
-			}
+                } else {}
+            }
 
-		}
+        }
 
-		rootScope.$broadcast("STATE_CHANGED_HANDLER", {});
-	});
+        rootScope.$broadcast("STATE_CHANGED_HANDLER", {});
+    });
 };
 
-apiconn.response_received_handler = function(jo) {
+apiconn.response_received_handler = function (jo) {
 
-	rootScope.$apply(function() {
+    rootScope.$apply(function () {
 
-		if (jo.ustr != null && jo.ustr != "" && jo.uerr != "ERR_CONNECTION_EXCEPTION"){
-            layer.msg(jo.ustr,{icon:2})
-            callBackFn[jo.obj+'_'+jo.act].shift()
-		} else{
-            if(callBackFn[jo.obj+'_'+jo.act]&&callBackFn[jo.obj+'_'+jo.act].length){
-                     callBackFn[jo.obj+'_'+jo.act].shift()(jo)
+        if (jo.ustr != null && jo.ustr != "" && jo.uerr != "ERR_CONNECTION_EXCEPTION") {
+            layer.msg(jo.ustr, {
+                icon: 2
+            })
+            callBackFn[jo.obj + '_' + jo.act].shift()
+        } else {
+            if (callBackFn[jo.obj + '_' + jo.act] && callBackFn[jo.obj + '_' + jo.act].length) {
+                callBackFn[jo.obj + '_' + jo.act].shift()(jo)
             }
-		}
-		if (jo.obj == "person" && jo.act == "login" && jo.user_info && jo.server_info) {
-			// goto_view("i001");
+        }
+        if (jo.obj == "person" && jo.act == "login" && jo.user_info && jo.server_info) {
+            // goto_view("i001");
             // window.location.reload()
             // goto_view("/i203");
-		}
-		if (jo.obj == "person" && jo.act == "logout") {
-			goto_view("/i203");
-			return;
-		}
+        }
+        if (jo.obj == "person" && jo.act == "logout") {
+            goto_view("/i203");
+            return;
+        }
 
-		// $scope.$on("RESPONSE_RECEIVED_HANDLER", function(event, jo) {}
-		if (jo.obj == "sdk" && jo.act == "switchreq") {
-			return goto_view(jo.ixxx);
-		}
+        // $scope.$on("RESPONSE_RECEIVED_HANDLER", function(event, jo) {}
+        if (jo.obj == "sdk" && jo.act == "switchreq") {
+            return goto_view(jo.ixxx);
+        }
 
-		rootScope.$broadcast("RESPONSE_RECEIVED_HANDLER", jo);
-	});
+        rootScope.$broadcast("RESPONSE_RECEIVED_HANDLER", jo);
+    });
 };
 
 apiconn.wsUri = "ws://39.108.219.7:51717/znyx";
 angular.module("iweb")
-    .factory('fileReader', function($q, $log) {
+    .factory('fileReader', function ($q, $log) {
 
-        var onLoad = function(reader, deferred, scope) {
+        var onLoad = function (reader, deferred, scope) {
             return function () {
                 /*scope.$apply(function () {
                     deferred.resolve(reader.result);
@@ -242,7 +243,7 @@ angular.module("iweb")
                 });*/
             };
         };
-        var getReader = function(deferred) {
+        var getReader = function (deferred) {
             var reader = new FileReader();
             reader.onload = onLoad(reader, deferred);
             reader.onerror = onError(reader, deferred);
@@ -255,18 +256,18 @@ angular.module("iweb")
             return deferred.promise;
         };
         var callFun = null;
-        var openFile = function(callback){
+        var openFile = function (callback) {
             var ele = document.getElementById("uploadImageFile");
-            if(ele){
+            if (ele) {
                 document.body.removeChild(ele);
             }
             ele = document.createElement("input");
-            ele.setAttribute("type","file");
-            ele.setAttribute("id","uploadImageFile");
+            ele.setAttribute("type", "file");
+            ele.setAttribute("id", "uploadImageFile");
             ele.style.display = "none";
             document.body.appendChild(ele);
             callFun = callback;
-            ele.onchange=function(event){
+            ele.onchange = function (event) {
                 //附件预览
                 var file = (event.srcElement || event.target).files[0];
                 getFile(file);
@@ -275,45 +276,45 @@ angular.module("iweb")
         };
 
         //打开文件
-        var getFile = function(file){
-            uploadFile('local_file',file,200)
+        var getFile = function (file) {
+            uploadFile('local_file', file, 200)
         };
 
         //上传文件
-        var uploadFile = function(src, data, sizes) {
+        var uploadFile = function (src, data, sizes) {
             var fd = new FormData();
             fd.append("local_file", data);
             fd.append("proj", apiconn.server_info.proj);
             fd.append("sizes", sizes);
             var xhr = new XMLHttpRequest();
-            xhr.addEventListener("load",uploadComplete, false);
+            xhr.addEventListener("load", uploadComplete, false);
             xhr.open("POST", apiconn.server_info.upload_to);
             xhr.send(fd);
         };
         //上传完成
-        var uploadComplete = function(evt) {
-            var jo ={};
-            if(evt.target.responseText.indexOf("} ")>0){
-                jo = JSON.parse(evt.target.responseText.substring(0,evt.target.responseText.lastIndexOf(" ")));
-            }else{
+        var uploadComplete = function (evt) {
+            var jo = {};
+            if (evt.target.responseText.indexOf("} ") > 0) {
+                jo = JSON.parse(evt.target.responseText.substring(0, evt.target.responseText.lastIndexOf(" ")));
+            } else {
                 // jo = JSON.parse(evt.target.responseText);
-                jo = eval('('+evt.target.responseText+')');
+                jo = eval('(' + evt.target.responseText + ')');
             }
             var img = new Image();
             var count = 10;
-            img.onerror = function() {
+            img.onerror = function () {
                 if (count > 0) {
                     count--;
                 }
             };
-            if(callFun){
+            if (callFun) {
                 callFun(jo);
             }
         };
 
         return {
             readAsDataUrl: readAsDataURL,
-            openFile:openFile
+            openFile: openFile
         };
 
     })
@@ -328,28 +329,35 @@ angular.module("iweb")
                 }
             }
         }
-    }).directive("ordertr",function(){
-    return {
-        restrict: 'E',
-        replace: true,
-        template: '<tr><td>123</td></tr>',
-        scope: {
-            obj: '@obj'
-        }
-    };
-});
+    }).directive("ordertr", function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<tr><td>123</td></tr>',
+            scope: {
+                obj: '@obj'
+            }
+        };
+    });
 
 iweb.run(['$rootScope', function ($rootScope) {
-	$rootScope.$on("$routeChangeSuccess", function(angularEvent, current, previous) { //
-		if(current.controller=="main"){
-			$rootScope.showMenu = false;
-		}else{
-			$rootScope.showMenu = true;
-		}
-	 });
+    $rootScope.$on("$routeChangeSuccess", function (angularEvent, current, previous) { //
+        if (current.controller == "main") {
+            $rootScope.showMenu = false;
+        } else {
+            $rootScope.showMenu = true;
+        }
+        if (current.controller == "i101") {
 
-	rootScope = $rootScope;
+        } else {
+            if (window.visibilitychangeCall) {
+                window.isjiantong = false;
+                document.removeEventListener("visibilitychange", window.visibilitychangeCall, false);
+                window.visibilitychangeCall = null;
+            }
+        }
+    });
+
+    rootScope = $rootScope;
     apiconn.connect();
 }]);
-
-
--- i101.js
+++ i101.js
@@ -1,33 +1,69 @@
-iweb.controller('i101',function($scope,$routeParams){
-    $scope.ctrlScope = $scope
-	$scope.live = ($routeParams.live=='true')?true:false;
-	$scope.talk_id ="";
-	$scope.header_id = "o15598664532377779483";
-	$scope.messages = [];
-	$scope.message=''
-	$scope.userInfo = {};
-	$scope.send = function(){
-		ajax({
-			obj: "message",
-			act: "group_send",
-			header_id : $routeParams.talk,
-			mtype : "text",
-			content : $scope.message
-		},function(data){
-			$scope.messages.push({
-				mtype: "text",
-				content : $scope.message,
-				from_name : $scope.userInfo.display_name
-			})
-            setTimeout(function(){
-                $(".i101-talk-content").scrollTop(100000000);
-            },100)
-            $scope.message=''
-			//$(".i101-talk-content").scrollTop($('.i101-talk-content')[0].scrollHeight);
-		})
-	}
+iweb.controller('i101', function ($scope, $routeParams) {
 
-    $scope.initPlayer=function () {
+    $scope.ctrlScope = $scope
+    $scope.live = ($routeParams.live == 'true') ? true : false;
+    $scope.talk_id = "";
+    $scope.header_id = "o15598664532377779483";
+    $scope.messages = [];
+    $scope.message = ''
+    $scope.userInfo = {};
+
+    window.visibilitychangeCall = function () {
+        var state = document.visibilityState;
+        if (state == "hidden") {
+            //离开选项卡
+            updateZhuanzhu("不专注");
+        } else if (state == "visible") {
+            //返回选项卡
+            updateZhuanzhu("专注");
+        }
+        console.log("切换", document.visibilityState);
+    }
+    if ($scope.live) {
+        function updateZhuanzhu(type) {
+            if (location.hash.match("/i101?")) {
+                //专注度接口  user updatestatus 数据采集 # 38 直播数据采集 TOP↑
+                ajax({
+                    obj: 'user',
+                    act: 'updatestatus',
+                    courseid: $routeParams.courseid, //课程id   p_user_coursetable 的 courseid
+                    watchid: $routeParams.watchid, //子课程id  p_user_coursetable的 watchid
+                    time: parseInt(Date.now() / 1000), //unix时间戳
+                    status: type, //专注，不专注，离线
+                }, function (data) {
+
+                });
+            }
+        }
+        if (!window.isjiantong) {
+            window.isjiantong = true;
+            document.addEventListener("visibilitychange", window.visibilitychangeCall, false);
+        }
+    }
+
+
+    $scope.send = function () {
+        ajax({
+            obj: "message",
+            act: "group_send",
+            header_id: $routeParams.talk,
+            mtype: "text",
+            content: $scope.message
+        }, function (data) {
+            $scope.messages.push({
+                mtype: "text",
+                content: $scope.message,
+                from_name: $scope.userInfo.display_name
+            })
+            setTimeout(function () {
+                $(".i101-talk-content").scrollTop(100000000);
+            }, 100)
+            $scope.message = ''
+            //$(".i101-talk-content").scrollTop($('.i101-talk-content')[0].scrollHeight);
+        })
+    }
+
+    $scope.initPlayer = function () {
         function getPathParams() {
             var path = location.pathname.split('/');
             if (path[1] == 'vod-player') {
@@ -43,9 +79,9 @@ iweb.controller('i101',function($scope,$routeParams){
 
         var rtmp = $routeParams.rtmp,
             flv = $routeParams.flv,
-            m3u8 = $routeParams.m3u8+'?txSecret='+$routeParams.txSecret+'&txTime='+$routeParams.txTime,
-            mp4 =  $routeParams.mp4,
-            live = ($routeParams.live=='true')?true:false ,
+            m3u8 = $routeParams.m3u8 + '?txSecret=' + $routeParams.txSecret + '&txTime=' + $routeParams.txTime,
+            mp4 = $routeParams.mp4,
+            live = ($routeParams.live == 'true') ? true : false,
             coverpic = $routeParams.coverpic,
             // width = getParams('width') $routeParams,
             // height = getParams('height') $routeParams,
@@ -53,7 +89,7 @@ iweb.controller('i101',function($scope,$routeParams){
             // flash = (getParams('flash') == 'true' ? true : false),
             // x5_player = (getParams('x5_player') == 'false' ? false : true),
             // h5_flv = (getParams('h5_flv') == 'true' ? true : false),
-            autoplay = ($routeParams.autoplay=='true')?true:false,
+            autoplay = ($routeParams.autoplay == 'true') ? true : false,
             flashUrl = (function () {
                 var params = getPathParams();
                 if (params) {
@@ -61,9 +97,9 @@ iweb.controller('i101',function($scope,$routeParams){
                 }
                 return '//imgcache.qq.com/open/qcloud/video/player/release/QCPlayer.swf'
             })()
-            // log = getParams('log');
+        // log = getParams('log');
 
-        console.log(m3u8,'aaaaaaaaaaaaaaaaaaaaaaaa')
+        console.log(m3u8, 'aaaaaaaaaaaaaaaaaaaaaaaa')
         /**
          * 视频类型播放优先级
          * mobile ：m3u8>mp4
@@ -77,9 +113,9 @@ iweb.controller('i101',function($scope,$routeParams){
             mp4: mp4,
             autoplay: true,
             live: $scope.live,
-            width:  '860',
-            height:  '572',
-            volume:  0.5,
+            width: '860',
+            height: '572',
+            volume: 0.5,
             // flash:true,
             // flash: flash,
             // flashUrl: '//imgcache.qq.com/open/qcloud/video/player/release/QCPlayer.swf',
@@ -88,7 +124,7 @@ iweb.controller('i101',function($scope,$routeParams){
             wording: {
                 2032: '请求视频失败，请检查网络',
                 2048: '请求m3u8文件失败，可能是网络错误或者跨域问题',
-                2:'直播还未开始，稍后刷新试试'
+                2: '直播还未开始，稍后刷新试试'
             },
             listener: function (msg) {
 
@@ -97,59 +133,59 @@ iweb.controller('i101',function($scope,$routeParams){
         window.tcplayer = new TcPlayer('video-container', options);
     }
     $scope.initPlayer()
-	$scope.getData=function () {
+    $scope.getData = function () {
 
-        if(apiconn.conn_state==='IN_SESSION'){
-        	ajax({
-                obj:'user',
-                act:'readmyinfo',
-           },function (data) {
-                $scope.userInfo=data.info
+        if (apiconn.conn_state === 'IN_SESSION') {
+            ajax({
+                obj: 'user',
+                act: 'readmyinfo',
+            }, function (data) {
+                $scope.userInfo = data.info
                 $scope.download_domain = apiconn.server_info.download_path
-                $scope.regionData={
-                    areaprovince:data.info.areaprovince,
-                    areacity:data.info.areacity,
-                    areacounty:data.info.areacounty
+                $scope.regionData = {
+                    areaprovince: data.info.areaprovince,
+                    areacity: data.info.areacity,
+                    areacounty: data.info.areacounty
                 }
                 $scope.getMessages()
             });
             // getMessages();
         }
-      }
-      setTimeout(function () {
-          $scope.getData()
-      },500)
+    }
+    setTimeout(function () {
+        $scope.getData()
+    }, 500)
 
-    $scope.$on("STATE_CHANGED_HANDLER", function() {
-        if(apiconn.conn_state==='IN_SESSION'){
+    $scope.$on("STATE_CHANGED_HANDLER", function () {
+        if (apiconn.conn_state === 'IN_SESSION') {
             $scope.getData()
         }
     })
-	$scope.$on("RESPONSE_RECEIVED_HANDLER",function(event,jo){
+    $scope.$on("RESPONSE_RECEIVED_HANDLER", function (event, jo) {
 
 
-		if(jo.obj == "push" && jo.act == "message_group" ){
-   			var msg ={
-   				mtype : jo.mtype,
-   				content : jo.content,
-   				from_name : jo.from_name,
-   				from_avatar : from_avatar
-   			}
-			$scope.messages.push(msg);
-		}
-	});
-    $scope.getMessages=function(){
-    		ajax({
-            	obj:'message',
-            	act:'group_get',
-            	header_id:$routeParams.talk
-           },function(data){
-            	if (data.info.entries.length > 0){
-            		$scope.messages=data.info.entries;
-            		setTimeout(function(){
-                        $(".i101-talk-content").scrollTop(100000000);
-                    },100)
-            	}
-            })
+        if (jo.obj == "push" && jo.act == "message_group") {
+            var msg = {
+                mtype: jo.mtype,
+                content: jo.content,
+                from_name: jo.from_name,
+                from_avatar: from_avatar
+            }
+            $scope.messages.push(msg);
+        }
+    });
+    $scope.getMessages = function () {
+        ajax({
+            obj: 'message',
+            act: 'group_get',
+            header_id: $routeParams.talk
+        }, function (data) {
+            if (data.info.entries.length > 0) {
+                $scope.messages = data.info.entries;
+                setTimeout(function () {
+                    $(".i101-talk-content").scrollTop(100000000);
+                }, 100)
+            }
+        })
     }
 })
